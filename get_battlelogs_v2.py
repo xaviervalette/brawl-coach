9e9feed029f2d0e205cb0d12f81f319418894d84
Xavier VALETTE <xaviervalette@outlook.com>
2021-09-22 14:32:57 +0200

    Remove recursive looping

--- get_battlelogs_v2.py
+++ get_battlelogs_v2.py
@@ -83,13 +83,13 @@ def get_history_from_tag(tags,token,country_code):
 		response = requests.request("GET", "https://api.brawlstars.com/v1/players/"+url_tag+"/battlelog", headers=headers, data=payload)
 		#print(response)
 		battlelogs.append(response.json())
-		with open(filename, 'w') as fp:
-    			json.dump(battlelogs, fp)
+	with open(filename, 'w') as fp:
+    		json.dump(battlelogs, fp)
 
 token=read_api_token("token.txt")
 
 for country_code in country_code_list:
-	print(country_code)
+	print(country_code+"_"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 	ranks=get_rank_from_api(token,country_code)
 	tags=get_tag_from_rank(ranks)
 	history=get_history_from_tag(tags,token, country_code)
