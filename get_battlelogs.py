d6a563a7b44b5d701d44bed8fb22c0c9a4b7585f
Xavier VALETTE <xaviervalette@outlook.com>
2021-09-25 23:39:52 +0200

    Battlelogs_20210925

--- get_battlelogs.py
+++ get_battlelogs.py
@@ -3,6 +3,32 @@ import requests
 import datetime
 
 home="/home/ec2-user/brawl-tier/"
+"""
+country_code_list=["AF", "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR",
+"AM", "AW", "AU", "AT", "AZ", "BS", "BH", "BD", "BB", "BY", "BE",
+"BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO",
+"BN", "BG", "BF", "BI", "CV", "KH", "CM", "CA", "KY", "CF", "TD",
+"CL", "CN", "CX", "CC", "CO", "KM", "CG", "CD", "CK", "CR", "CI",
+"HR", "CU", "CW", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG",
+"SV", "GQ", "ER", "EE", "ET", "FK", "FO", "FJ", "FI", "FR", "GF",
+"PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", "GR", "GL", "GD",
+"GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM", "VA", "HN",
+"HK", "HU", "IS", "IN", "ID", "IR", "IQ", "IE", "IM", "IL", "IT",
+"JM", "JP", "JE", "JO", "KZ", "KE", "KI", "KP", "KR", "KW", "KG",
+"LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MK",
+"MG", "MW", "MY", "MV", "ML", "MT", "MH", "MQ", "MR", "MU", "YT",
+"MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", "NA",
+"NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU", "NF", "MP",
+"NO", "OM", "PK", "PW", "PS", "PA", "PG", "PY", "PE", "PH", "PN",
+"PL", "PT", "PR", "QA", "RE", "RO", "RU", "RW", "BL", "SH", "KN",
+"LC", "MF", "PM", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC",
+"SL", "SG", "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES",
+"LK", "SD", "SR", "SJ", "SZ", "SE", "CH", "SY", "TW", "TJ", "TZ",
+"TH", "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV",
+"UG", "UA", "AE", "GB", "US", "UM", "UY", "UZ", "VU", "VE", "VN",
+"VG", "VI", "WF", "EH", "YE", "ZM", "ZW"]
+"""
+country_code_list=sorted(["FR", "US", "DE", "IT", "ES", "JP"])
 
 def read_api_token(filename):
 	"""
@@ -15,19 +41,33 @@ def read_api_token(filename):
 	infile.close()
 	return data.strip()
 
-def get_rank_from_api(token):
+def get_event_from_api(token):
+	filename = home+"/events/events_"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+".json"
+        url = "https://api.brawlstars.com/v1/events/rotation"
+        headers = {
+        'Authorization':'Bearer '+token
+        }
+        payload={}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        events=response.json()
+        with open(filename, 'w') as fp:
+                json.dump(events, fp)
+
+def get_rank_from_api(token,country_code):
 	"""
 	Desc:   GET tags of leaderboard
 	Input:  API token
 	Output: A 200 player json file
 	"""
-	url = "https://api.brawlstars.com/v1/rankings/FR/players"
+	ranks={}
+	url = "https://api.brawlstars.com/v1/rankings/"+country_code+"/players"
 	headers = {
 	'Authorization':'Bearer '+token
 	}
 	payload={}
 	response = requests.request("GET", url, headers=headers, data=payload)
 	ranks=response.json()
+	#print(response)
 	return ranks
 
 def get_tag_from_rank(rank):
@@ -41,15 +81,14 @@ def get_tag_from_rank(rank):
         	tags.append(i["tag"])
 	return tags
 
-def get_history_from_tag(tags,token):
+def get_history_from_tag(tags,token,country_code):
 	"""
 	Desc:   GET battlelogs from tag list
 	Input:  A list of tags
 	Output: A battlelogs json file of the tags in input
 	"""
 	battlelogs=[]
-	filename = home+"/battlelogs/battlelogs_"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+".json"
-	#tags_first=[tags[0], tags[1]]
+	filename = home+"/battlelogs/battlelogs_"+country_code+"_"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+".json"
 	for tag in tags:
 		headers = {
 	  	'Authorization': 'Bearer '+token
@@ -57,12 +96,15 @@ def get_history_from_tag(tags,token):
 		url_tag=tag.replace("#","%23")
 		payload={}
 		response = requests.request("GET", "https://api.brawlstars.com/v1/players/"+url_tag+"/battlelog", headers=headers, data=payload)
-		print(response)
+		#print(response)
 		battlelogs.append(response.json())
-		with open(filename, 'w') as fp:
-    			json.dump(battlelogs, fp)
+	with open(filename, 'w') as fp:
+    		json.dump(battlelogs, fp)
 
 token=read_api_token("token.txt")
-ranks=get_rank_from_api(token)
-tags=get_tag_from_rank(ranks)
-history=get_history_from_tag(tags, token)
+get_event_from_api(token)
+for country_code in country_code_list:
+	print(country_code+"_"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+	ranks=get_rank_from_api(token,country_code)
+	tags=get_tag_from_rank(ranks)
+	history=get_history_from_tag(tags,token, country_code)
---
+++ get_best_teams.py
@@ -0,0 +1,84 @@
+import json
+from collections import defaultdict
+import os
+import glob
+base_dir = '/home/ec2-user/brawl-tier/battlelogs/battlelogs_20210925-2/*'
+
+with open("events/events_20210925-151745.json") as f:
+    events = json.load(f)
+
+target_event=events[2]
+print(target_event)
+#battelogs[player_number{0-200}]["items"][game_number{0-25}]["event"]["id"]
+
+winning_team=[]
+loosing_team=[]
+
+
+battlelogs_list=[]
+filepaths = glob.glob(base_dir)
+for fp in filepaths:
+        with open(fp, 'r') as f:
+                # Read the first line
+                battlelogs = json.load(f)
+                # Append the first line into the list
+                battlelogs_list.append(battlelogs)
+game_number_targeted_event=0
+for battlelogs in battlelogs_list:
+        for player in battlelogs:
+                for game in player["items"]:
+                        #print(game["event"]["id"])
+			if(game["event"]["id"]==target_event["event"]["id"]):
+				game_number_targeted_event=game_number_targeted_event+1
+				star_player=game["battle"]["starPlayer"]
+				if star_player in game["battle"]["teams"][0]:
+					winning_team_index=False
+				else:
+					winning_team_index=True
+				winners=[]
+				loosers=[]
+				for brawler in game["battle"]["teams"][winning_team_index]:
+					winners.append(brawler["brawler"]["name"])
+				for brawler in game["battle"]["teams"][not(winning_team_index)]:
+					loosers.append(brawler["brawler"]["name"])
+				winners=sorted(winners)
+				winning_team.append(winners)
+				loosers=sorted(loosers)
+				loosing_team.append(loosers)
+
+print "Number of game corresponding to the targeted event:",game_number_targeted_event
+
+def remove_team_duplicate(team):
+	team_no_dupplicate=[]
+	for elem in team:
+		if elem not in team_no_dupplicate:
+			team_no_dupplicate.append(elem)
+	return team_no_dupplicate
+
+winning_team_no_dupplicate=remove_team_duplicate(winning_team)
+loosing_team_no_dupplicate=remove_team_duplicate(loosing_team)
+
+win_table=[]
+for team in winning_team_no_dupplicate:
+	#win_table.append([winning_team.count(team),team])
+	if(loosing_team.count(team)==0):
+		win_rate=1
+	else:
+		win_rate=float(winning_team.count(team))/float((winning_team.count(team)+loosing_team.count(team)))
+	win_dict = {
+		"teamStats": {
+			"winNumbers": winning_team.count(team),
+			"winRate":round(win_rate,3),
+			"pickRate": round((winning_team.count(team)+loosing_team.count(team))/float(game_number_targeted_event),3),
+			"pickNumber":winning_team.count(team)+loosing_team.count(team),
+			"brawlers": team
+			}
+	}
+	win_table.append(win_dict)
+	#print("Team:",team,"Count",winning_team.count(team))
+
+print(win_table[0])
+
+win_table = sorted(win_table, key=lambda win_table: win_table['teamStats']["winNumbers"], reverse=True)
+with open("win_table.json", 'w') as fp:
+	json.dump(win_table, fp)
