957ecf849d20add7d1c7e649531a706eda675b01
Xavier VALETTE <xaviervalette@outlook.com>
2022-01-16 22:48:40 +0100

    change team picker front and images

--- python/getImages.py
+++ python/getImages.py
@@ -1,38 +1,63 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import os
+import errno
 
 
-site = 'https://brawlify.com/maps/'
+sites = ['https://brawlify.com/gamemodes/detail/Duels', 
+'https://brawlify.com/gamemodes/detail/Hot-Zone', 
+'https://brawlify.com/gamemodes/detail/Knockout', 
+'https://brawlify.com/gamemodes/detail/Gem-Grab', 
+'https://brawlify.com/gamemodes/detail/Showdown', 
+'https://brawlify.com/gamemodes/detail/Duo-Showdown', 
+'https://brawlify.com/gamemodes/detail/Bounty', 
+'https://brawlify.com/gamemodes/detail/Brawl-Ball', 
+'https://brawlify.com/gamemodes/detail/Siege']
+
+modes=["DUELS", "HOTZONE", "KNOCKOUT", "GEMGRAB", "SOLOSHOWDOWN", "DUOSHOWDOWN", "BOUNTY", "BRAWLBALL", "SIEGE"]
 #site = "https://brawlify.com/gamemodes/detail/Duels"
-response = requests.get(site)
+i=0
+for site in sites:
+    response = requests.get(site)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-img_tags = soup.find_all('img')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    img_tags = soup.find_all('img')
 
-urls = [img['src'] for img in img_tags]
+    urls = [img['src'] for img in img_tags]
 
+    for url in urls:
+        filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+        if not filename:
+            print("Regex didn't match with the url: {}".format(url))
+            continue
+        print(url)
+        if(modes[i]=="DUOSHODOWN"):
+            pictureName=pictureName.replace(" DUO","").upper()
+        pictureName=filename.group(1)
+        pictureName=pictureName.replace("-"," ").upper()
+        pictureName=pictureName.replace(" 2","")
+        pictureName=pictureName.replace("'","")
+        pictureName=pictureName.replace(".PNG",".JPG")
+        pictureName=pictureName.upper()
 
-for url in urls:
-    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
-    if not filename:
-         print("Regex didn't match with the url: {}".format(url))
-         continue
-    print(url)
-    pictureName=filename.group(1)
-    pictureName=pictureName.replace("-"," ").upper()
-    pictureName=pictureName.replace(" 2","")
-    pictureName=pictureName.replace("'","")
-    pictureName=pictureName.replace(".PNG",".JPG")
-    pictureName=pictureName.upper()
+        print(pictureName)
 
-    print(pictureName)
+        filename="../web/static/img/maps/"+modes[i]+"/"+pictureName
 
-    with open("../web/static/img/maps/"+pictureName, 'wb') as f:
-        if 'http' not in url:
-            # sometimes an image source can be relative 
-            # if it is provide the base url which also happens 
-            # to be the site variable atm. 
-            url = '{}{}'.format(site, url)
-        response = requests.get(url)
-        f.write(response.content)
+        if not os.path.exists(os.path.dirname(filename)):
+            try:
+                os.makedirs(os.path.dirname(filename))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+
+        with open(filename, 'wb') as f:
+            if 'http' not in url:
+                # sometimes an image source can be relative 
+                # if it is provide the base url which also happens 
+                # to be the site variable atm. 
+                url = '{}{}'.format(site, url)
+            response = requests.get(url)
+            f.write(response.content)
+    i=i+1
