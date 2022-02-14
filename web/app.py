f347888b737ad9db7f76a9dcbc32b23d4b40238b
Xavier VALETTE <xaviervalette@outlook.com>
2022-02-14 11:27:29 +0100

    correct wrong table size of soloBest

--- web/app.py
+++ web/app.py
@@ -18,78 +18,6 @@ def homepage():
 		return redirect(url_for('tag_results', tag=tag))
 	return render_template('home.html')
 
-@app.route("/explore/modes")
-def modes():
-	return render_template('modes.html')
-
-@app.route("/tag_reader/<string:tag>")
-def tag_results(tag):
-	if(tag==""):
-		return redirect(url_for('tag_not_found'))
-
-	battlelogs = getPlayerStats(token, tag)
-	try:
-		if battlelogs["reason"]=="notFound":
-			return redirect(url_for('tag_not_found'))
-	except:
-		#return battlelogs
-		looseNumber=0
-		winNumber=0
-		drawNumber=0
-		starNumber=0
-		result=[]
-		for battle in battlelogs['items']:
-			print(battle)
-			try:
-				if battle["battle"]["starPlayer"]["tag"]==tag:
-					battle.update({"isStarPlayer":"yes"})
-					starNumber=starNumber+1
-				else:
-					battle.update({"isStarPlayer":"no"})
-			except:
-					battle.update({"isStarPlayer":"N/A"})
-			
-			try:
-				if battle["battle"]["result"]=="victory":
-					winNumber=winNumber+1
-					result.append("victory")
-				elif battle["battle"]["result"]=="defeat":
-					looseNumber=looseNumber+1
-					result.append("defeat")
-				else:
-					drawNumber=drawNumber+1
-					result.append("draw")
-
-			except:
-				try:
-					if battle["battle"]["rank"]<5:
-						winNumber=winNumber+1
-						result.append("victory")
-					elif battle["battle"]["rank"]>5:
-						looseNumber=looseNumber+1
-						result.append("defeat")
-					else:
-						drawNumber=drawNumber+1
-						result.append("draw")
-				except:
-					result.append("N/A")
-
-
-		return render_template('tag_results.html', battlelogs=battlelogs, len=len(battlelogs["items"]), tag=tag, starNumber=starNumber, winNumber=winNumber, looseNumber=looseNumber, drawNumber=drawNumber, result=result)
-
-@app.route("/tag_reader/tag_not_found")
-def tag_not_found():
-	return "TO DO - NOT FOUND"
-
-@app.route("/tag_reader/")
-def tag_reader():
-	return render_template('tag_reader.html')
-
-
-@app.route("/modes/<string:mode>/maps")
-def modes_maps(mode):
-		return mode
-
 @app.route("/currentMeta")
 def team_picker():
 	battleNumber={}
@@ -128,23 +56,21 @@ def team_picker():
 
 @app.route("/currentMeta/<string:events>")
 def mode_map(events):
-	try:
-		current_events = readCurrentEvents("../events/current_events.json")
-		current_event=current_events[int(events)]
-		bestTeamsRaw, battleNum =readEventsStats(current_event, "teams")
-		bestSoloRaw, battleNum =readEventsStats(current_event, "solo")
+	current_events = readCurrentEvents("../events/current_events.json")
+	current_event=current_events[int(events)]
+	bestTeamsRaw, battleNum =readEventsStats(current_event, "teams")
+	bestSoloRaw, battleNum =readEventsStats(current_event, "solo")
 
-		bestTeamsSorted = sorted(bestTeamsRaw, key=lambda d: d['teamStats']["pickRate"], reverse=True)
-		bestSoloSorted = sorted(bestSoloRaw, key=lambda d: d['soloStats']["pickRate"], reverse=True)
+	bestTeamsSorted = sorted(bestTeamsRaw, key=lambda d: d['teamStats']["pickRate"], reverse=True)
+	bestSoloSorted = sorted(bestSoloRaw, key=lambda d: d['soloStats']["pickRate"], reverse=True)
 
-		if len(bestTeamsSorted)>50:
-			team_number=50
-		else:
-			team_number=len(bestTeamsSorted)
-		return render_template('currentMetaDetails.html', bestTeams=bestTeamsSorted, bestSolo=bestSoloSorted, len=team_number, mode=current_event["event"]["mode"], map=current_event["event"]["map"])
-
-	except:
-		print("to do")
+	if len(bestTeamsSorted)>50:
+		lenTeams=50
+	else:
+		lenTeams=len(bestTeamsSorted)
+		
+	lenSolo=len(bestSoloSorted)
+	return render_template('currentMetaDetails.html', bestTeams=bestTeamsSorted, bestSolo=bestSoloSorted, lenTeams=lenTeams, lenSolo=lenSolo, mode=current_event["event"]["mode"], map=current_event["event"]["map"])
 
 
 @app.route("/processTime.log")
